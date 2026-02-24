bea0147bb257679123c36ebd02eca545817c4cec
Godwin <godwinabiri456@gmail.com>
2026-02-24 15:25:47 +0100

    new commit

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="bg-gray-800 text-white py-6">
+      <div className="container mx-auto px-4">
+        <p className="text-center">© 2023 My Next.js App. All rights reserved.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
