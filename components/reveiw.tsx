0cf8c2fcdc055856362cb8ce68db3973b950737b
Godwin <godwinabiri456@gmail.com>
2026-02-25 22:44:02 +0100

    new

---
+++ components/reveiw.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const reveiw = () => {
+
+    return (
+      <div>
+        <div>
+            <h1> Read reviews,</h1>
+            <h1>Learning made easy</h1>
+            <h6>See how our courses have empowered learners to reach new milestones</h6>
+        </div>
+        <div>
+             
+        </div>
+        
+      </div>
+    )
+  }
+
+export default reveiw
