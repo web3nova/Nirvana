859305451dc01b623ac3849fbb748bf6e9e1ce39
Godwin <godwinabiri456@gmail.com>
2026-02-26 16:10:36 +0100

    new

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
+import Reveiw from "@/components/reveiw";
+import Hero from "@/components/hero";
 
 export default function Home() {
   return (
     <div>
+      <div className="hero-container">
       <Navbar />
+      <Hero/>
+      </div>
+      <Reveiw />
 
-      <main className="container mx-auto px-4 py-8">
-        <h1 className="text-4xl font-bold mb-4">Welcome to My Next.js App</h1>
-        <p className="text-lg text-gray-700">
-          This is a simple Next.js application with a navbar and footer.
-        </p>
-      </main>
+
       <Footer />
     </div>
   );
--- components/reveiw.tsx
+++ components/reveiw.tsx
@@ -10,8 +10,35 @@ const reveiw = () => {
             <h6>See how our courses have empowered learners to reach new milestones</h6>
         </div>
         <div>
-             
+            <img src="image r1.jpg" alt="review 1" />
         </div>
+        <div>
+            <div>
+                <img src="image r2.jpg" alt="review 2" />
+            </div>
+            <div>
+                <img src="image r3.jpg" alt="review 3" />
+            </div>
+        </div>
+        <div>
+            <img src="image r4.jpg" alt="review 4" />
+        </div>
+        <div>
+            <div>
+                <img src="image r5.jpg" alt="review 5" />
+            </div>
+            <div>
+                <span>"</span>
+                <h4>From graduates who've transformed their careers </h4>
+            </div>
+            <div>
+                <img src="image r6.jpg" alt="review 6" />
+            </div>
+        </div>
+        <div>
+            div
+        </div>
+
         
       </div>
     )
